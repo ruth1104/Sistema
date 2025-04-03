28a30287067f397042ed75d2c1101a8b172ba1d7
ruth1104 <ruthdu30@gmail.com>
2025-04-02 21:30:44 -0500

    subir proyecto

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+import secrets
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///flaskdb.sqlite'
+    #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root@localhost:3306/sistema'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = secrets.token_urlsafe(24)
